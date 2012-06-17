2c8056092231d0dc58cf5477d664fcff597d22e2
Manohar Akula <manohar.akula@gmail.com>
2012-06-17 21:21:28 +0530

    linked_by field can be specified for nested fields using dot as seperator

--- lib/query.rb
+++ lib/query.rb
@@ -21,10 +21,15 @@ class Query
 
   def execute target_doc
     @docs.each do |doc|
-      if doc[@linked_field] == target_doc['_id']
-        return doc[@value_field]
+      if access_field(doc, @linked_field) == target_doc['_id']
+        return access_field(doc, @value_field)
       end
     end
     nil
+  end
+
+  private
+  def access_field doc, accessor
+    eval 'doc' << accessor.split('.').map { |field| "['#{field}']" }.to_s
   end
 end
--- spec/copy_field_spec.rb
+++ spec/copy_field_spec.rb
@@ -32,3 +32,47 @@ describe 'copy field from one doc type to another: ' do
     @foobar.get(@foo_doc3['id'])['name'].should == nil
   end
 end
+
+describe 'copy field where the target field is nested deep inside: ' do
+  before do
+    @foo_doc = @foobar.save_doc({ 'type' => 'foo' })
+    @fuu_doc = @foobar.save_doc({ 'type' => 'fuu', 'profile' => { 'identity' => { 'name' => 'attila' }}, 'foo_id' => @foo_doc['id'] })
+
+    class CopyName < Migration
+      on_database 'foobar'
+
+      up do
+        over_scope 'foobar/all_foo' do
+          add 'name', query('foobar/all_fuu').linked_by('foo_id').field('profile.identity.name')
+        end
+      end
+    end
+    Migrator.run CopyName
+  end
+
+  it "should retrieve and add name" do
+    @foobar.get(@foo_doc['id'])['name'].should == 'attila'
+  end
+end
+
+describe 'copy field where the target field is linked by a field nested deep inside: ' do
+  before do
+    @foo_doc = @foobar.save_doc({ 'type' => 'foo' })
+    @fuu_doc = @foobar.save_doc({ 'type' => 'fuu', 'name' => 'attila', 'foo' => { 'id' => @foo_doc['id'] }})
+
+    class CopyName < Migration
+      on_database 'foobar'
+
+      up do
+        over_scope 'foobar/all_foo' do
+          add 'name', query('foobar/all_fuu').linked_by('foo.id').field('name')
+        end
+      end
+    end
+    Migrator.run CopyName
+  end
+
+  it "should retrieve and add name" do
+    @foobar.get(@foo_doc['id'])['name'].should == 'attila'
+  end
+end
