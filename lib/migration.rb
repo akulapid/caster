a574fd600080d26bf96de632fef3748faa48f4b3
Manohar Akula <manohar.akula@gmail.com>
2012-06-18 16:05:52 +0530

    refactoring - operations are now executed in the context of scopes

--- lib/migration.rb
+++ lib/migration.rb
@@ -1,11 +1,4 @@
 require 'scope'
-require 'op/add'
-require 'op/remove'
-require 'op/rename'
-require 'op/create'
-require 'op/delete'
-require 'ref/cross_reference'
-require 'ref/self_reference'
 
 class Migration
 
@@ -23,40 +16,7 @@ class Migration
     @up_scopes
   end
 
-  def self.over_scope view, query = {}
-    @current_scopes << Scope.new(@database_name, view, query)
-    yield
-  end
-
-  def self.query scope, query = {}
-    CrossReference.new @database_name, scope, query
-  end
-
-  def self.field accessor
-    SelfReference.new accessor
-  end
-
-  def self.add field, value
-    @current_scopes.last.add_operation(Add.new(field, value))
-  end
-
-  class << self
-    alias_method :update, :add
-  end
-
-  def self.remove field
-    @current_scopes.last.add_operation(Remove.new(field))
-  end
-
-  def self.rename old_name, new_name
-    @current_scopes.last.add_operation(Rename.new(old_name, new_name))
-  end
-
-  def self.create params
-    @current_scopes.last.add_operation(Create.new(params))
-  end
-
-  def self.delete
-    @current_scopes.last.add_operation(Delete.new)
+  def self.over_scope view, query = {}, &block
+    @current_scopes << Scope.new(@database_name, view, query, &block)
   end
 end
--- lib/scope.rb
+++ lib/scope.rb
@@ -1,17 +1,51 @@
 require 'couchrest'
+require 'op/add'
+require 'op/remove'
+require 'op/rename'
+require 'op/create'
+require 'op/delete'
+require 'ref/cross_reference'
+require 'ref/self_reference'
 
 # defines an execution scope which is a set of documents over which migration operations run
 class Scope
 
-  def initialize database_name, view, query
+  def initialize database_name, view, query, &block
+    @database_name = database_name
     @db = CouchRest.database "http://127.0.0.1:5984/#{database_name}"
     @view = view
     @query = query
     @operations = []
+    instance_eval &block
   end
 
-  def add_operation operation
-    @operations << operation
+  def add field, value
+    @operations << Add.new(field, value)
+  end
+  alias_method :update, :add
+
+  def remove field
+    @operations << Remove.new(field)
+  end
+
+  def rename old_name, new_name
+    @operations << Rename.new(old_name, new_name)
+  end
+
+  def create params
+    @operations << Create.new(params)
+  end
+
+  def delete
+    @operations << Delete.new
+  end
+
+  def query scope, query = {}
+    CrossReference.new @database_name, scope, query
+  end
+
+  def field accessor
+    SelfReference.new accessor
   end
 
   def execute
@@ -25,5 +59,5 @@ class Scope
       docs << doc
     end
     @db.bulk_save docs
-  end
+  end  
 end
