f2ab22b996c331302d5b6bb09e8f05c7cf25e791
Manohar Akula <manohar.akula@gmail.com>
2012-07-14 20:14:53 +0530

    some minor refactoring

--- spec/view_scope_spec.rb
+++ spec/view_scope_spec.rb
@@ -44,6 +44,27 @@ describe 'view with include docs' do
   end
 end
 
+describe 'query scope' do
+  before do
+    @foo_loc1 = @foobar.save_doc({ 'loc' => 'foo' })
+    @foo_loc2 = @foobar.save_doc({ 'loc' => 'foo' })
+    @fuu_loc = @foobar.save_doc({ 'loc' => 'fuu' })
+
+    over 'foobar/foobar/by_loc', { 'key' => 'foo' } do
+      add 'name', 'atilla'
+    end
+  end
+
+  it "should add name field to all docs with loc = foo" do
+    @foobar.get(@foo_loc1['id'])['name'].should == 'atilla'
+    @foobar.get(@foo_loc2['id'])['name'].should == 'atilla'
+  end
+
+  it "should not add name field to doc with loc != foo" do
+    @foobar.get(@fuu_loc['id'])['name'].should == nil
+  end
+end
+
 describe 'refer scoped documents' do
   before do
     @doc = @foobar.save_doc({ 'type' => 'foo', 'name' => 'attila', 'stats' => { 'score' => 5 }})
@@ -62,4 +83,3 @@ describe 'refer scoped documents' do
     @foobar.get(@doc['id'])['victories'].should == 5
   end
 end
-
